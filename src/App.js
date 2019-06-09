c60a1c99165aaf127d3e9958683b5f4cda7b8e72
bmanek <41875460+bmanek@users.noreply.github.com>
2019-06-09 15:01:14 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,38 @@
 // create your App component here
+
+import React, { Component } from 'react'
+export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      peopleInSpace: []
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.peopleInSpace.map(person => person.name)}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+}
+
+
+// This lab is missing the App component that index.js is looking for. To pass the tests in this lab:
+//
+// Create an App class component from scratch
+// Write a componentDidMount method. Inside the method, send a fetch request to http://api.open-notify.org/astros.json, a free API that returns a list of people currently in space.
+
+// Initial answer:
